c4ae0fb6fc0997dce25325b4269047654272ebad
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2017-12-28 19:52:50 -0500

    Created action creater, reducer for async fetching of posts. Rendered posts as a list.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-export default class PostIndex extends Component {
+import { fetchPosts } from '../actions';
+
+class PostIndex extends Component {
+  componentDidMount() {
+    // data fetched is async hence willmount or didmount wont make a difference
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    });
+  }
+  
   render () {
     return (
       <div>
-        Posts Index
+        <h1>Posts</h1>
+        <ul className="list-group">
+          {this.renderPosts()};
+        </ul>
       </div>
     )
   }
 }
+
+// Map redux state to Component level
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
