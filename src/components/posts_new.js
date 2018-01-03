652e6a0188645e2e5b66d9ba1f1e18c20852897e
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2018-01-03 16:44:19 -0500

    new posts redux-form validation and error styling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,18 +2,75 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render(){
+  renderField(field) {
+    const { meta: {touched, error}, label, input } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <form action="">
-        <Field
-          name="title"
-          component={}
+      <div className={className}>
+        <label>{label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...input}
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render(){
+    // handleSubmit is a redux-form property that deals with the clientside error handling
+    // it takes a callback (onSubmit) that performs our backend functions.
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button className="btn btn-primary" type="submit">Save</button>
       </form>
     )
   }
 }
 
+// Called when form is submitted
+function validate(values) {
+  // console.log(values) -> {title:'ads', categories: 'sdfsdf', content: 'ssdfsdf'}
+  const errors = {};
+
+  // validate inputs from 'values'
+  if(!values.title || values.title.length < 3)
+    errors.title = "Enter a title that is at least 3 characters";
+  if(!values.categories)
+    errors.categories = "Y u no add categories?!";
+  if(!values.content)
+    errors.content = "Enter some content foo!";
+  // if errors is empty, form is fine to sunbmit
+  // if any properties, redux-forms assumes invalid
+  return errors;
+}
+
 export default reduxForm({
-  form: 'PostsNewForm'
+  form: 'PostsNewForm',
+  validate
 })(PostsNew);
