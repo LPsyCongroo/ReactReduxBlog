c3985996b546942eb7786c76c336d57b353bfc8d
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2018-01-04 13:50:44 -0500

    added delete functionality.

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostShow extends Component {
   componentDidMount() {
-    // Get the id from the URL using react-router props
-    // params lists all the wildcard tokens in the url
-    const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    // If a user navigates from the homepage, they can just use the list of posts
+    // However a direct link to a post is used, then a get request is sent.
+    if(!this.props.post){
+      // Get the id from the URL using react-router props
+      // params lists all the wildcard tokens in the url
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;    
+    // call delete action creator
+    // don't pass id as this.props.post.id because it assumes we've fetched it. We may not have!
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
   }
 
   render() {
@@ -19,6 +33,11 @@ class PostShow extends Component {
 
     return (
       <div>
+        <Link to="/" className="btn btn-warning">Back</Link>
+        <button 
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >Delete</button>
         <h1>{post.title}</h1>
         <h5>Categories: {post.categories}</h5>
         <p>{post.content}</p>
@@ -31,4 +50,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,4 @@
-import { FETCH_POSTS } from '../actions';
-import { FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state ={}, action) {
@@ -10,7 +9,15 @@ export default function(state ={}, action) {
       return { ...state, [action.payload.data.id]: action.payload.data};
 
     case FETCH_POSTS:
+      // _.mapKeys takes an array of objects and, using a key from each, maps it to a new object
       return _.mapKeys(action.payload.data, 'id');
+    
+    case DELETE_POST:
+      // While the list of posts is refreshed after FETCH_POSTS,    
+      // If there is any single posts in the state from FETCH_POST
+      // it's good practice to update state to make UI more snappy.      
+      // _.omit looks for the key and returns an object without it.
+      return _.omit(state, action.payload);
 
     default:
       return state;
